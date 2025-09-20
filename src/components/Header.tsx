79d14e2897a26741be17c5effb77b80644cf5898
Ksenyia Kravchuk <kkravchuk@uma.tech>
2025-09-20 13:52:12 +0200

    Add a button Add new task

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Add } from "@mui/icons-material";
 import { AppBar, Button, Toolbar } from "@mui/material";
 
 export default function Header() {
@@ -5,6 +6,7 @@ export default function Header() {
       <Toolbar>
         <Button variant="outlined" color="inherit" href="/">Board</Button>
         <Button variant="outlined" color="inherit" href="/backlog" sx={{ ml: 2 }}>Back Log</Button>
+        <Button variant="outlined" color="inherit" href="#" sx={{ ml: 2 }} startIcon={<Add />}>New Task</Button>
       </Toolbar>
     </AppBar>;
   }
